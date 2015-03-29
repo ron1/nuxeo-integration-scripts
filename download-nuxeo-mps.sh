d99bd1b9669aa20dec012aa75c0121c9aea5427b
ron1 <ron1@localhost>
2015-03-29 15:26:37 -0400

    include source zips in marketplace downloads

--- download-nuxeo-mps.sh
+++ download-nuxeo-mps.sh
@@ -14,7 +14,7 @@ if [[ ! -e ${MARKETPLACE_PKG_LST_FILE} ]]; then
 fi
 MARKETPLACE_PKG_LST_FILE=`echo $(cd $(dirname "$MARKETPLACE_PKG_LST_FILE") && pwd -P)/$(basename "$MARKETPLACE_PKG_LST_FILE")`
 
-DOWNLOAD_DIR=./nxr-downloads
+DOWNLOAD_DIR=./nxr-downloads-${VERSION}
 if [ -e ${DOWNLOAD_DIR} ]; then
     echo "ERROR: ${DOWNLOAD_DIR} directory must not exist."
     exit 1
@@ -23,6 +23,8 @@ fi
 read -e -p "Enter Connect Username: " -i rg1 USERNM
 read -e -s -p "Enter Connect Password: " PASSWD
 CONNECT_BASE_URL="https://connect.nuxeo.com/nuxeo/site/marketplace/package"
+
+GITHUB_NUXEO_BASE_URL="https://github.com/nuxeo"
 
 PWD_FILE=$(mktemp)
 printf "password=%s" ${PASSWD} >> ${PWD_FILE}
@@ -42,6 +44,8 @@ while read p; do
             ${CONNECT_BASE_URL}/${PKG_NAME}-${PKG_VERSION}/download/\@\@login
     curl -s --cookie $COOKIE_JAR --cookie-jar $COOKIE_JAR -J -O \
             ${CONNECT_BASE_URL}/${PKG_NAME}-${PKG_VERSION}/download?version=${PKG_VERSION}
+    curl -s -L -o ${PKG_REPO_NAME}-${PKG_VERSION}-sources.zip \
+            ${GITHUB_NUXEO_BASE_URL}/${PKG_REPO_NAME}/archive/release-${PKG_VERSION}.zip
 
 done < ${MARKETPLACE_PKG_LST_FILE}
 
